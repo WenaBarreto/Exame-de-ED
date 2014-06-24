e22803972d99c713c737e0a4f96a2b044da9d63f
Raniel Sousa <worldtech15@hotmail.com>
2014-06-23 23:10:20 -0300

    teste

--- Exame final ED/src/br/com/facto/ED/Arvore.java
+++ Exame final ED/src/br/com/facto/ED/Arvore.java
@@ -1,7 +1,9 @@
 package br.com.facto.ED;
 
+import Aula05_Arvores.No;
+
 /**
- * @author Raniel Sousa classe Arvore 
+ * @author Raniel Sousa classe Arvore
  * */
 public class Arvore {
 	No raiz;
@@ -26,10 +28,11 @@ public class Arvore {
 	public No inserir(No novo, No inicio) {
 
 		if (inicio != null) {
-			if (novo.C.nome.equals(inicio.C.nome))// ACHO QUE O "compareTO" deve se
-											// ussado aqui para inserir em ondem
-			 								// afabeitica
-				{
+			if (novo.C.nome.equals(inicio.C.nome))// ACHO QUE O "compareTO" deve
+													// se
+			// ussado aqui para inserir em ondem
+			// afabeitica
+			{
 				inicio.esq = inserir(novo, inicio.esq);
 			} else {
 				inicio.dir = inserir(novo, inicio.dir);
@@ -46,19 +49,84 @@ public class Arvore {
 
 		if (raiz != null) {
 			imprimir(raiz.esq);
-			System.out.println("Nome:" + " " + raiz.nome + "  " + "Telefone:"
-					+ " " + raiz.telefone);
+			System.out.println("Nome:" + " " + raiz.C.nome + "  " + "Telefone:"
+					+ " " + raiz.C.telefone);
 			imprimir(raiz.dir);
+		}
+		
+		
+		
+		
+	}
+	
+	public No remover(No raiz, Agenda contatos)
+	{
+		if(raiz != null)
+		{
+			if(contatos.nome.compareTo( raiz.C.nome)) 
+			{
+				raiz.esq = remover(raiz.esq, contatos.nome);
+			}else{
+				if(contatos.nome.compareTo(raiz.C.nome) )
+				{
+					raiz.dir = remover(raiz.dir, contados.nome);
+				}else
+				{
+					if(raiz.esq != null && raiz.dir != null)//Interiores
+					{
+						raiz.C.nome = encontraMinimo(raiz.dir).contotos.nome;
+						raiz.dir = removerMinimo(raiz.dir);
+					}else
+					{
+						if(raiz.esq == null)
+						{
+							raiz = raiz.dir;
+						}else
+						{ 
+							raiz = raiz.esq;
+						}
+					}
+				}
+			}
+		}
+		return raiz;	
+	}
+	
+	public No encontraMinimo(No raiz)
+	{
+		if(raiz != null){
+			while(raiz.esq != null){
+				raiz = raiz.esq;
+			}
+		}
+		return raiz;
+	}
+	
+	public No removerMinimo(No raiz)
+	{
+		if(raiz == null)
+		{
+			return null;
+		}else{
+			if(raiz.esq != null){
+				raiz.esq = removerMinimo(raiz.esq);
+				return raiz;
+			}else{
+				return raiz.dir;
+			}
 		}
 	}
 
+	
+
 	// METODO E PARA TESTE A CLASSE, MAS SERA REMOVIDO DEPOIS
 	public static void main(String[] args) {
-		No contato;
+		Agenda C = new Agenda();
 		Arvore A = new Arvore();
-		A.imprimir(A.raiz);
+		
+		
+		
 
 	}
-	
 
 }
--- Exame final ED/src/br/com/facto/ED/Start.java
+++ Exame final ED/src/br/com/facto/ED/Start.java
@@ -4,10 +4,55 @@ import javax.swing.JOptionPane;
 
 public class Start {
 	static int menu;
+	String nome, fone;
+
+	public void menu() {
+		Arvore A = new Arvore();
+		Agenda C = new Agenda();
+
+		menu = Integer.parseInt(JOptionPane
+				.showInputDialog(" Excolha uma das opcões \n" + " 1-Inserir \n"
+						+ " 2- Pesquisar \n" + " 3-Pemover \n"
+						+ " 4 Imprimir \n" + " 5 Sair"));
+
+		switch (menu) {
+		case 1:
+			nome = JOptionPane.showInputDialog(null, "Informe o nome");
+			fone = JOptionPane.showInputDialog(null, "Infome o telefone");
+			
+			C.nome = nome;
+			C.telefone = fone;
+			A.insere(C);
+			
+		
+
+			
+
+			A.imprimir(A.raiz);
+
+			break;
+		case 2:
+			Start s = new Start();
+			s.menu02();
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+			System.exit(0);
+			break;
+
+		default:
+			break;
+		}
+
+	}
 
 	public void menu01() {
-		Arvore a = new Arvore();
-		a.insere("ranieu", "(63)8110-7595");
 
 		menu = Integer.parseInt(JOptionPane.showInputDialog("1-Arvore \n"
 				+ "2-Lista"));
@@ -15,48 +60,15 @@ public class Start {
 
 			switch (menu) {
 			case 1:
-				
 
 				break;
 			case 2:
-				a.imprimir(a.raiz);
 
 				break;
 
 			default:
 				break;
 			}
-		}
-		if (menu == 2) {
-
-			menu = Integer.parseInt(JOptionPane
-					.showInputDialog(" Excolha uma das opcões \n"
-							+ " 1-Inserir \n" + " 2- Pesquisar \n"
-							+ " 3-Pemover \n" + " 4 Imprimir \n" + " 5 Sair"));
-
-			switch (menu) {
-			case 1:
-
-				break;
-			case 2:
-				Start s = new Start();
-				s.menu02();
-
-				break;
-			case 3:
-
-				break;
-			case 4:
-
-				break;
-			case 5:
-				System.exit(0);
-				break;
-
-			default:
-				break;
-			}
-
 		}
 
 	}
@@ -67,7 +79,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		Start S = new Start();
-		S.menu01();
+		S.menu();
 	}
 
 }
